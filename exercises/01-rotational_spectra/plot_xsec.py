fe21840dbcf26244151912398f4ce68883c405e3
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2016-10-21 09:40:43 +0000

    * exercises/01-rotational_spectra/plot_xsec.py:
    Python script to demonstrate the usage of typhon to load ARTS XML files.
    The script loads and plots the absorption cross sections for the first
    exercise.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/arts-lectures/trunk@10082 aaf1aab0-4228-0410-ad68-8dceda47f409

---
+++ exercises/01-rotational_spectra/plot_xsec.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2016 Lukas Kluft <lukas.kluft@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""Load and plot absorption cross sections corresponding
+to arts output from "absorption.arts".
+"""
+from typhon.arts import xml
+from typhon.plots import styles
+import matplotlib.pyplot as plt
+
+
+# Load the ARTS results.
+abs_xsec_all = xml.load('results/abs_xsec_per_species.xml')
+abs_xsec = abs_xsec_all[0]
+freq = xml.load('results/f_grid.xml')
+species = xml.load('results/species.xml')
+mol_name = species[0][0].replace('-*', '')
+temperature = xml.load('results/temp.xml')
+pressure = xml.load('results/press.xml')
+
+# Store setup information in dict.
+kwargs = {
+    'n': mol_name,
+    'p': pressure / 1e2,
+    'T': temperature,
+    }
+
+# Plot the results.
+plt.style.use(styles('typhon'))
+
+fig, ax = plt.subplots()
+ax.plot(freq / 1e9, abs_xsec)
+ax.set_xlabel('Frequency [GHz]')
+ax.set_ylabel('Abs. cross section [$m^2$]')
+ax.set_title('{n} p:{p:0.0f} hPa T:{T:0.0f} K'.format(**kwargs))
+
+# Save figure.
+fig.savefig('plots/plot_xsec_{n}_{p:0.0f}hPa_{T:.0f}K.pdf'.format(**kwargs))
