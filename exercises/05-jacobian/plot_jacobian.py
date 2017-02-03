8ccfe0c6fc1576585404ee7037025cd9275234ce
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2017-02-03 11:53:26 +0000

    * exercises/05-jacobian/plot_jacobian.py:
    Added author to file header.
    
    PEP8 fixes.
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/arts-lectures/trunk@10277 aaf1aab0-4228-0410-ad68-8dceda47f409

--- exercises/05-jacobian/plot_jacobian.py
+++ exercises/05-jacobian/plot_jacobian.py
@@ -1,27 +1,25 @@
-#==========================================================================
-#NAME: plot_jacobian
+# -*- coding: utf-8 -*-
 #
-#      this script uses the output variables of the arts controlfile
-#      "jacobian.arts". it plots nadir brightness temperature at the top of
-#      the atmosphere, zenith opacity, the water vapour jacobian and the
-#      opacity between altitude z and the top of the atmosphere. Different
-#      frequencies can be selected by changing the variable "freq_ind" into
-#      a number between 1 and 110.
-#
-#==========================================================================
+# Author: Felix Erdmann
+"""This script uses the output variables of the arts controlfile
+`jacobian.arts`. it plots nadir brightness temperature at the top of
+the atmosphere, zenith opacity, the water vapour jacobian and the
+opacity between altitude z and the top of the atmosphere. Different
+frequencies can be selected by changing the variable `freq_ind` into
+a number between 1 and 110."""
 
 
 import matplotlib
 matplotlib.use('Agg')  # Use Agg-backend to produce plots without X-Server
 import matplotlib.pyplot as plt
 from typhon.arts import xml
-from typhon.plots import styles
 import numpy as np
 
-## select frequency
+# select frequency
 freq_ind = -1
+freq_ind = 100
 
-## read in everything
+# read in everything
 freq = xml.load('./results/f_grid.xml')
 iy_aux = xml.load('./results/iy_aux.xml')
 opacity = xml.load('./results/y_aux.xml')
@@ -34,13 +32,13 @@ press = iy_aux[0]
 abs_ = iy_aux[1]
 opacity = opacity[0]
 
-## calculate the level where opacity becomes one
+# calculate the level where opacity becomes one
 nlevls = len(alt)
 nfreqs = len(freq)
 
-## flipping the altitude dimension to start the integration from TOA
-abs_ = abs_[:,::-1]
-z_fine = alt[::-1,:]
+# flipping the altitude dimension to start the integration from TOA
+abs_ = abs_[:, ::-1]
+z_fine = alt[::-1, :]
 z_chapmann = np.zeros((nfreqs, 1))
 
 for ifreq in range(nfreqs):
@@ -52,14 +50,13 @@ for ifreq in range(nfreqs):
         if tau > 1:
             z_chapmann[ifreq, 0] = z_fine[ilev] / 1000.0
             break
-## plotting ...
 
-
-plt.figure(1)#'PaperPosition', [.25 .25 8 6]);
+# plotting ...
+plt.figure(1)
 if freq_ind > 0:
-  plt.subplot(221)
+    plt.subplot(221)
 else:
-  plt.subplot(211)
+    plt.subplot(211)
 plt.plot(freq / 10**9, bt)
 plt.xlabel('Frequency [ GHz ]')
 plt.ylabel(r'T$_B$ [ K ]')
@@ -70,12 +67,12 @@ if freq_ind > 0:
     plt.text(152, 280, str((freq[freq_ind]/10**9).round(3))+'GHz')
 
 if freq_ind > 0:
-  plt.subplot(222)
+    plt.subplot(222)
 else:
-  plt.subplot(212)
+    plt.subplot(212)
 plt.semilogy(freq / 10**9, opacity)
-plt.xlabel('Frequency [ GHz ]');
-plt.ylabel('Zenith Opacity [  ]');
+plt.xlabel('Frequency [ GHz ]')
+plt.ylabel('Zenith Opacity [  ]')
 plt.title('Zenith Opacity')
 
 if freq_ind > 0:
@@ -83,17 +80,17 @@ if freq_ind > 0:
 
 if freq_ind > 0:
     plt.subplot(223)
-    
-    
-    plt.plot(jac[freq_ind, :], alt[:,0,0] / 1000.0)
+
+    plt.plot(jac[freq_ind, :], alt[:, 0, 0] / 1000.0)
     plt.xlim([-1, 7])
     plt.ylim([0, 70])
     plt.xlabel('Jacobian [ K / 1 ]')
     plt.ylabel('Altitude [ km ]')
     plt.title('Water Vapor Jacobian')
-    plt.text(4, 60, str(alt[np.argmin(jac[freq_ind, :]),0,0].round(1)/1000)+'km')
+    plt.text(4, 60, str(
+        alt[np.argmin(jac[freq_ind, :]), 0, 0].round(1) / 1000) + 'km')
 
-    ## Calculate opacity between each level and TOA
+    # Calculate opacity between each level and TOA
     opac_layer_space = np.zeros((nlevls, 1))
     abs_per_freq = np.squeeze(abs_[freq_ind, :])
 
@@ -103,30 +100,26 @@ if freq_ind > 0:
         tau = tau + abs_layer * (z_fine[ilev] - z_fine[ilev + 1])
         opac_layer_space[ilev + 1, 0] = tau
 
-
     plt.subplot(224)
-    plt.semilogx(opac_layer_space[:,0], z_fine[:,0,0] / 1000.0)
-    plt.xlim([10**-13,10**3])
-    plt.xticks([10.**-13,10.**-10,10.**-5,10.**0,10.**2])
-    plt.ylim([0,70])
+    plt.semilogx(opac_layer_space[:, 0], z_fine[:, 0, 0] / 1000.0)
+    plt.xlim([10**-13, 10**3])
+    plt.xticks([10.**-13, 10.**-10, 10.**-5, 10.**0, 10.**2])
+    plt.ylim([0, 70])
     plt.xlabel(r'Opacity$_{layer--space}$ [  ]')
     plt.ylabel('Altitude [ km ]')
     plt.title('Opacity from a layer to space')
     try:
-       plt.text(0.01, 60, str(z_fine[np.where(opac_layer_space[:,0]>=1)[0][0],0,0].round(1)/1000)+'km')
+        plt.text( 0.01, 60, str(z_fine[np.where(opac_layer_space[:, 0] >= 1)[0][0], 0, 0].round(1)/1000)+'km')
     except Exception:
-       print('no opacity greater than 1')
+        print('no opacity greater than 1')
 
-    #plot([1, 1], [0, 20], '--')
-
-## save figure
-
-pathname = './plots/';
+# save figure
+pathname = './plots/'
 if freq_ind < 0:
-    figname = 'bt_op_part1.png'
+    figname = 'bt_op_part1.pdf'
     plt.tight_layout()
-    plt.savefig(pathname+figname, dpi=300)
+    plt.savefig(pathname + figname)
 else:
-    figname = 'jac_'+str(np.round((freq[freq_ind] / 10**9))) +'GHz.png'
+    figname = 'jac_' + str(np.round((freq[freq_ind] / 10**9))) + 'GHz.pdf'
     plt.tight_layout()
-    plt.savefig(pathname+figname, dpi=300)
+    plt.savefig(pathname+figname)
