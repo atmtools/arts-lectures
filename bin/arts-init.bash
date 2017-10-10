5c26c924110b4a764a38c8a2b49e67455926dc43
olemke <olemke@aaf1aab0-4228-0410-ad68-8dceda47f409>
2017-10-10 12:20:37 +0000

    * bin/arts_init.bash: Adapted for new computer pool.
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/arts-lectures/trunk@10697 aaf1aab0-4228-0410-ad68-8dceda47f409

--- bin/arts-init.bash
+++ bin/arts-init.bash
@@ -8,20 +8,21 @@
 
 # Set environment variables where to find ...
 # ... the arts binary,
-PATH="/scratch/lehre/rtcourse/arts/build/src:$PATH"
+PATH="/data/share/lehre/unix/rtcourse/arts/build/src:$PATH"
 # ... ARTS auxiliary data,
-ARTS_DATA_PATH="/scratch/lehre/rtcourse/arts-xml-data:$ARTS_DATA_PATH"
-ARTS_DATA_PATH="/scratch/lehre/rtcourse/catalogue:$ARTS_DATA_PATH"
+ARTS_DATA_PATH="/data/share/lehre/unix/rtcourse/arts-xml-data:$ARTS_DATA_PATH"
+ARTS_DATA_PATH="/data/share/lehre/unix/rtcourse/catalogue:$ARTS_DATA_PATH"
+ARTS_INCLUDE_PATH="/data/share/lehre/unix/rtcourse/arts/controlfiles:$ARTS_INCLUDE_PATH"
 
-export PATH ARTS_DATA_PATH MATLABPATH
+export PATH ARTS_DATA_PATH ARTS_INCLUDE_PATH MATLABPATH
 
 # Limit ARTS CPU usage to four cores.
 export OMP_NUM_THREADS=4
 
 # Load latest MATLAB version.
 module unload matlab && module load matlab/2016a
-MATLABPATH="/scratch/lehre/rtcourse/atmlab/atmlab:$MATLABPATH"
+MATLABPATH="/data/share/lehre/unix/rtcourse/atmlab/atmlab:$MATLABPATH"
 
 # Setup Python 3 environent (using Anaconda).
 module unload python
-export PATH="/scratch/lehre/rtcourse/anaconda3/bin:$PATH"
+export PATH="/data/share/lehre/unix/rtcourse/anaconda3/bin:$PATH"
