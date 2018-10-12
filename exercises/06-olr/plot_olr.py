eec0eb83e075259530f4a2f599ad77ea46fdd4e6
lkluft <lkluft@aaf1aab0-4228-0410-ad68-8dceda47f409>
2018-10-12 09:09:19 +0000

    Fix typhon deprecation warning.
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/arts-lectures/trunk@11110 aaf1aab0-4228-0410-ad68-8dceda47f409

--- exercises/06-olr/plot_olr.py
+++ exercises/06-olr/plot_olr.py
@@ -20,7 +20,7 @@ plt.style.use(typhon.plots.styles('typhon'))
 
 fig, ax = plt.subplots()
 temps = [225, 250, 275, t_surf]
-temp_colors = typhon.plots.mpl_colors('temperature', len(temps))
+temp_colors = typhon.plots.cmap2rgba('temperature', len(temps))
 for t, color in sorted(zip(temps, temp_colors)):
     ax.plot(wn, typhon.physics.planck(f, t),
             label='{:3.1f} K'.format(t),
@@ -28,9 +28,9 @@ for t, color in sorted(zip(temps, temp_colors)):
 ax.plot(wn, olr, label='Radiance', color='steelblue')
 ax.legend(framealpha=0.5)
 ax.grid(True)
-ax.set_title(r'OLR = {:3.2f} $Wm^{{-2}}Sr^{{-1}}$'.format(np.trapz(olr, f)))
+ax.set_title(r'OLR={:3.2f} $\sf Wm^{{-2}}Sr^{{-1}}$'.format(np.trapz(olr, f)))
 ax.set_xlim(wn.min(), wn.max())
-ax.set_xlabel(r'Wave number [$cm^{-1}$]')
-ax.set_ylabel(r'Radiance [$Wm^{-2}Hz^{-1}Sr^{-1}$]')
+ax.set_xlabel(r'Wave number [$\sf cm^{-1}$]')
+ax.set_ylabel(r'Radiance [$\sf Wm^{-2}Hz^{-1}Sr^{-1}$]')
 ax.set_ylim(0, ax.get_ylim()[1])
 fig.savefig('plots/olr{}.pdf'.format(scene))
