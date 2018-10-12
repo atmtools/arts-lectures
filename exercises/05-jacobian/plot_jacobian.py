Fix Jacobian exercises.
* exercises/05-jacobian/jacobian.arts:

Adapt the controfile to latest ARTS version (arts-2-3-983 and above).

The new implementation of `iyEmissionStandard` introduced in
arts-2-3-983 requires a modification of the controlfile. Absorption
coefficients as a function of frequency and pressure are no longer
available as auxiliary output. A customized agenda writes these
variables to an XML file to circumvent this issue.

* exercises/05-jacobian/plot_jacobian.py:

Adapt the plotting script to work with new ARTS output.

Minor modifications (e.g. visual guides)of the created plots.

* exercises/05-jacobian/plot_jacobian.m:

Remove inoperative Matlab plotting script.

* 05-jacobian/exe5_description.{docx,pdf}:

Remove mention of the Matlab plotting script.

git-svn-id: https://arts.mi.uni-hamburg.de/svn/rt/arts-lectures/trunk@11105 aaf1aab0-4228-0410-ad68-8dceda47f409

exercises/05-jacobian/plot_jacobian.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Author: Felix Erdmann
+# Based on a script authored by Felix Erdmann
 """This script uses the output variables of the arts controlfile
 `jacobian.arts`. it plots nadir brightness temperature at the top of
 the atmosphere, zenith opacity, the water vapour jacobian and the
@@ -17,35 +17,14 @@ freq_ind = -1
 
 # read in everything
 freq = xml.load('./results/f_grid.xml')
-iy_aux = xml.load('./results/iy_aux.xml')
-opacity = xml.load('./results/y_aux.xml')
-bt = xml.load('./results/bt.xml')
+p_grid = xml.load('./results/p_grid.xml')
+tau = xml.load('./results/optical_thickness.xml')
+bt = xml.load('./results/y.xml')
 jac = xml.load('./results/jacobian.xml')
 alt = xml.load('./results/z_field.xml')
-mol_name = xml.load('./results/species.xml')
 
-press = iy_aux[0]
-abs_ = iy_aux[1]
-opacity = opacity[0]
-
-# calculate the level where opacity becomes one
-nlevls = len(alt)
-nfreqs = len(freq)
-
-# flipping the altitude dimension to start the integration from TOA
-abs_ = abs_[:, ::-1]
-z_fine = alt[::-1, :]
-z_chapmann = np.zeros((nfreqs, 1))
-
-for ifreq in range(nfreqs):
-    tau = 0.0
-    abs_per_freq = np.squeeze(abs_[ifreq, :])
-    for ilev in range(nlevls - 1):
-        abs_layer = 0.5 * (abs_per_freq[ilev] + abs_per_freq[ilev + 1])
-        tau = tau + abs_layer * (z_fine[ilev] - z_fine[ilev + 1])
-        if tau > 1:
-            z_chapmann[ifreq, 0] = z_fine[ilev] / 1000.0
-            break
+# Find emission level based on "opcaity rule".
+z_chapman = alt[np.argmax(tau >= 1.0, axis=0)]
 
 # plotting ...
 plt.figure(1)
@@ -66,13 +45,14 @@ if freq_ind > 0:
     plt.subplot(222)
 else:
     plt.subplot(212)
-plt.semilogy(freq / 10**9, opacity)
+plt.semilogy(freq / 10**9, tau[-1, :])
+plt.axhline(1, color='black', linewidth=0.8, linestyle='dashed', zorder=-1)
 plt.xlabel('Frequency [ GHz ]')
 plt.ylabel('Zenith Opacity [  ]')
 plt.title('Zenith Opacity')
 
 if freq_ind > 0:
-    plt.plot(freq[freq_ind] / 10**9, opacity[freq_ind], 'or')
+    plt.plot(freq[freq_ind] / 10**9, tau[-1, freq_ind], 'or')
 
 if freq_ind > 0:
     plt.subplot(223)
@@ -83,31 +63,29 @@ if freq_ind > 0:
     plt.xlabel('Jacobian [ K / 1 ]')
     plt.ylabel('Altitude [ km ]')
     plt.title('Water Vapor Jacobian')
-    plt.text(4, 60, str(
-        alt[np.argmin(jac[freq_ind, :]), 0, 0].round(1) / 1000) + 'km')
+    jac_peak = alt[np.argmin(jac[freq_ind, :]), 0, 0].round(1) / 1000
+    plt.text(4, 60, f'{jac_peak:.2f} km')
+    plt.axhline(jac_peak, color='red', linestyle='dashed', linewidth=0.8)
 
-    # Calculate opacity between each level and TOA
-    opac_layer_space = np.zeros((nlevls, 1))
-    abs_per_freq = np.squeeze(abs_[freq_ind, :])
-
-    tau = 0
-    for ilev in range(nlevls - 1):
-        abs_layer = 0.5 * (abs_per_freq[ilev] + abs_per_freq[ilev + 1])
-        tau = tau + abs_layer * (z_fine[ilev] - z_fine[ilev + 1])
-        opac_layer_space[ilev + 1, 0] = tau
+    opac_layer_space = tau[:, [freq_ind]]
 
     plt.subplot(224)
-    plt.semilogx(opac_layer_space[:, 0], z_fine[:, 0, 0] / 1000.0)
+    plt.semilogx(opac_layer_space[:, 0], alt[::-1, 0, 0] / 1000.0)
     plt.xlim([10**-13, 10**3])
     plt.xticks([10.**-13, 10.**-10, 10.**-5, 10.**0, 10.**2])
     plt.ylim([0, 70])
-    plt.xlabel(r'Opacity$_{layer--space}$ [  ]')
+    plt.xlabel(r'$\sf Opacity_{layer-space}$ []')
     plt.ylabel('Altitude [ km ]')
     plt.title('Opacity from a layer to space')
     try:
-        plt.text(0.01, 60, str(z_fine[np.where(opac_layer_space[:, 0] >= 1)[0][0], 0, 0].round(1)/1000)+'km')
+        tau1 = alt[::-1][np.where(opac_layer_space[:, 0] >= 1)[0][0], 0, 0]
     except Exception:
         print('no opacity greater than 1')
+    else:
+        tau1 /= 1000
+        plt.text(0.01, 60, f'{tau1:.2f} km')
+        plt.axhline(tau1, color='red', linestyle='dashed', linewidth=0.8)
+        plt.axvline(1, ymax=0.2, color='black', linewidth=0.8, zorder=-1)
 
 # save figure
 pathname = './plots/'