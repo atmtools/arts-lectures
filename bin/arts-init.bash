06d6de9138bbaed909cce387820f0886d671557d
Lukas Kluft <lukas.kluft@gmail.com>
2019-10-22 08:42:04 +0200

    Adjust the bash initialisation script
    Add the `ARTS_BUILD_PATH` to locate the ARTS API.
    
    Major refactoring of the whole script.

--- bin/arts-init.bash
+++ bin/arts-init.bash
@@ -1,24 +1,20 @@
 #!/bin/bash
-#
-# arts-init.bash
-# Copyright (C) 2016 Lukas Kluft <lukas.kluft@gmail.com>
-#
-# Distributed under terms of the MIT license.
-#
 
 # Unload CEN-IT python modules because they break the PYTHONPATH
 module unload python3 python
 
+# Define base path to radiative transfer course
+RTPATH="/data/share/lehre/unix/rtcourse"
+
 # Set environment variables where to find ...
-# ... the arts binary,
-PATH="/data/share/lehre/unix/rtcourse/arts/build/src:$PATH"
-PATH="$PATH:/data/share/lehre/unix/rtcourse/bin"
-# ... ARTS auxiliary data,
-ARTS_DATA_PATH="/data/share/lehre/unix/rtcourse/arts-xml-data:$ARTS_DATA_PATH"
-ARTS_DATA_PATH="/data/share/lehre/unix/rtcourse/catalogue:$ARTS_DATA_PATH"
-ARTS_INCLUDE_PATH="/data/share/lehre/unix/rtcourse/arts/controlfiles:$ARTS_INCLUDE_PATH"
+# ... the ARTS API
+export ARTS_BUILD_PATH="$RTPATH/arts/build:$ARTS_BUILD_PATH"
 
-export PATH ARTS_DATA_PATH ARTS_INCLUDE_PATH MATLABPATH
+# ... and ARTS auxiliary data,
+export ARTS_DATA_PATH="$RTPATH/arts-xml-data:$ARTS_DATA_PATH"
+export ARTS_DATA_PATH="$RTPATH/catalogue:$ARTS_DATA_PATH"
+export ARTS_INCLUDE_PATH="$RTPATH/arts/controlfiles:$ARTS_INCLUDE_PATH"
 
-# Setup Python 3 environent (using Anaconda).
-export PATH="/data/share/lehre/unix/rtcourse/anaconda3/bin:$PATH"
+# Setup the user search path to include the ARTS binary
+# and our Anaconda Python Distribution.
+export PATH="$RTPATH/arts/build/src:$RTPATH/anaconda3/bin:$PATH:$RTPATH/bin"
