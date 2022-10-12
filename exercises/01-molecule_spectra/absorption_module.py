b6ec9c504fae3790d69ffa3b372fb3604700599a
Manfred Brath <manfred.brath@uni-hamburg.de>
2022-10-12 16:45:13 +0200

    Update lecture 1 and to latest ARTS 2.5.6

--- exercises/01-molecule_spectra/absorption_module.py
+++ exercises/01-molecule_spectra/absorption_module.py
@@ -3,7 +3,6 @@ import re
 
 import matplotlib.pyplot as plt
 import numpy as np
-import typhon as ty
 import pyarts
 from typhon.plots import styles
 
@@ -13,17 +12,17 @@ def tag2tex(tag):
     return re.sub("([a-zA-Z]+)([0-9]+)", r"\1$_{\2}$", tag)
 
 
-def calculate_absxsec(
-    species="N2O",
-    pressure=800e2,
-    temperature=300.0,
-    fmin=10e9,
-    fmax=2000e9,
-    fnum=10_000,
-    lineshape="LP",
-    normalization="RQ",
-    verbosity=0
-):
+def calculate_absxsec(species="N2O",
+                      pressure=800e2,
+                      temperature=300.0,
+                      fmin=10e9,
+                      fmax=2000e9,
+                      fnum=10_000,
+                      lineshape="LP",
+                      normalization="RQ",
+                      verbosity=0,
+                      vmr=0.05,
+                      lines_off=0):
     """Calculate absorption cross sections.
 
     Parameters:
@@ -35,46 +34,45 @@ def calculate_absxsec(
         fnum (int): Number of frequency grid points.
         lineshape (str): Line shape model.
                             Available options:
-    	                    DP   	 - 	 Doppler profile,
-	                        LP   	 - 	 Lorentz profile,
-	                        VP   	 - 	 Voigt profile,
-	                        SDVP 	 - 	 Speed-dependent Voigt profile,
-	                        HTP  	 - 	 Hartman-Tran profile.
+                            DP        -      Doppler profile,
+                            LP        -      Lorentz profile,
+                            VP        -      Voigt profile,
+                            SDVP      -      Speed-dependent Voigt profile,
+                            HTP       -      Hartman-Tran profile.
         normalization (str): Line shape normalization factor.
                             Available options:
-                            VVH  	 - 	 Van Vleck and Huber,
-                            VVW  	 - 	 Van Vleck and Weisskopf,
-                            RQ   	 - 	 Rosenkranz quadratic,
-                            None 	 - 	 No extra normalization.  
+                            VVH       -      Van Vleck and Huber,
+                            VVW       -      Van Vleck and Weisskopf,
+                            RQ        -      Rosenkranz quadratic,
+                            None      -      No extra normalization.
         verbosity (int): Set ARTS verbosity (``0`` prevents all output).
+        vmr (float): Volume mixing ratio. This is mainly important for the
+                     water vapor continua.
+        lines_off (int): Switch off lines, if no contnua is selected,
+                         absorption will be zero.
 
     Returns:
         ndarray, ndarray: Frequency grid [Hz], Abs. cross sections [m^2]
     """
     # Create ARTS workspace and load default settings
     ws = pyarts.workspace.Workspace(verbosity=0)
-    ws.execute_controlfile("general/general.arts")
-    ws.execute_controlfile("general/continua.arts")
-    ws.execute_controlfile("general/agendas.arts")
+    ws.LegacyContinuaInit()
+    ws.water_p_eq_agendaSet()
+    ws.PlanetSet(option="Earth")
+
     ws.verbositySetScreen(ws.verbosity, verbosity)
 
     # We do not want to calculate the Jacobian Matrix
     ws.jacobianOff()
 
-    # Agenda for scalar gas absorption calculation
-    ws.Copy(ws.abs_xsec_agenda, ws.abs_xsec_agenda__noCIA)
-
     # Define absorption species
     ws.abs_speciesSet(species=[species])
-    ws.ArrayOfIndexSet(ws.abs_species_active, [0])
-
-    ws.abs_lines_per_speciesReadSpeciesSplitCatalog(
-       basename="spectroscopy/Artscat/"
-    )
-
-    ws.abs_lines_per_speciesSetLineShapeType(option=lineshape)
-    ws.abs_lines_per_speciesSetCutoff(option="ByLine", value=750e9)
-    ws.abs_lines_per_speciesSetNormalization(option=normalization)
+    ws.abs_lines_per_speciesReadSpeciesSplitCatalog(basename='lines/')
+    ws.abs_lines_per_speciesLineShapeType(option=lineshape)
+    ws.abs_lines_per_speciesCutoff(option="ByLine", value=750e9)
+    ws.abs_lines_per_speciesNormalization(option=normalization)
+    if lines_off:
+        ws.abs_lines_per_speciesSetEmpty()
 
     # Create a frequency grid
     ws.VectorNLinSpace(ws.f_grid, fnum, fmin, fmax)
@@ -89,19 +87,22 @@ def calculate_absxsec(
     # Setting the pressure, temperature and vmr
     ws.rtp_pressure = float(pressure)  # [Pa]
     ws.rtp_temperature = float(temperature)  # [K]
-    ws.rtp_vmr = np.array([1.0])  # [VMR]
+    ws.rtp_vmr = np.array([vmr])  # [VMR]
     ws.Touch(ws.rtp_nlte)
 
     # isotop
     ws.isotopologue_ratiosInitFromBuiltin()
 
     # Calculate absorption cross sections
-    ws.propmat_clearsky_agenda_checked = 1
     ws.lbl_checkedCalc()
+    ws.propmat_clearsky_agenda_checked = 1
     ws.propmat_clearskyInit()
     ws.propmat_clearskyAddLines()
+    ws.propmat_clearskyAddConts()
 
     # Convert abs coeff to cross sections on return
-    number_density = pressure / (ty.constants.boltzmann * temperature)
-    return (ws.f_grid.value.copy(),
-            ws.propmat_clearsky.value.data.data[0, 0, :, 0].copy() / number_density)
+    number_density = pressure * vmr / (pyarts.arts.constant.k * temperature)
+
+    return (ws.f_grid.value.value.copy(),
+            ws.propmat_clearsky.value.data.value[0, 0, :, 0].copy() /
+            number_density)
