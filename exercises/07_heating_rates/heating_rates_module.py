cd21428c33f5f3b79482968b62ae0a71d6b3c1c1
Manfred Brath <manfred.brath@uni-hamburg.de>
2023-11-07 00:05:06 +0100

    Update exercise 7
    *Remove typhon dependencies.
    
    *Use Disort to calculate spectral irradiance

--- exercises/07_heating_rates/heating_rates_module.py
+++ exercises/07_heating_rates/heating_rates_module.py
@@ -1,11 +1,10 @@
 """Simulate and plot Earth's outgoing longwave radiation (OLR)."""
 import numpy as np
 import pyarts.workspace
-from typhon import physics as phys
 
 
 def calc_spectral_irradiance(atmfield,
-                             nstreams=2,
+                             nstreams=4,
                              fnum=300,
                              fmin=1.0,
                              fmax=97e12,
@@ -32,31 +31,14 @@ def calc_spectral_irradiance(atmfield,
     ws.water_p_eq_agendaSet()
     ws.gas_scattering_agendaSet()
     ws.PlanetSet(option="Earth")
+
     ws.verbositySetScreen(ws.verbosity, verbosity)
-
-    # (standard) emission calculation
-    ws.iy_main_agendaSet(option="Emission")
-
-    # cosmic background radiation
-    ws.iy_space_agendaSet(option="CosmicBackground")
-
-    # standard surface agenda (i.e., make use of surface_rtprop_agenda)
-    ws.iy_surface_agendaSet(option="UseSurfaceRtprop")
-
-    # sensor-only path
-    ws.ppath_agendaSet(option="FollowSensorLosPath")
-
-    # no refraction
-    ws.ppath_step_agendaSet(option="GeometricPath")
 
     # Number of Stokes components to be computed
     ws.IndexSet(ws.stokes_dim, 1)
 
-    # No jacobian calculation
+    # No jacobian calculations
     ws.jacobianOff()
-
-    # Clearsky = No scattering
-    ws.cloudboxOff()
 
     # Definition of species
     ws.abs_speciesSet(species=[
@@ -64,15 +46,17 @@ def calc_spectral_irradiance(atmfield,
         "CO2, CO2-CKDMT252"
     ])
 
-    # Read line catalog
+        # Read line catalog
     ws.abs_lines_per_speciesReadSpeciesSplitCatalog(basename="lines/")
 
     # Load CKDMT400 model data
     ws.ReadXML(ws.predefined_model_data, "model/mt_ckd_4.0/H2O.xml")
 
-    # ws.abs_lines_per_speciesLineShapeType(option=lineshape)
+    # Read cross section data
+    ws.ReadXsecData(basename="lines/")
+
     ws.abs_lines_per_speciesCutoff(option="ByLine", value=750e9)
-    # ws.abs_lines_per_speciesNormalization(option=normalization)
+    ws.abs_lines_per_speciesTurnOffLineMixing()
 
     # Create a frequency grid
     ws.VectorNLinSpace(ws.f_grid, int(fnum), float(fmin), float(fmax))
@@ -80,14 +64,15 @@ def calc_spectral_irradiance(atmfield,
     # Throw away lines outside f_grid
     ws.abs_lines_per_speciesCompact()
 
+    # Calculate absorption
+    ws.propmat_clearsky_agendaAuto()
+
     # Weakly reflecting surface
     ws.VectorSetConstant(ws.surface_scalar_reflectivity, 1, 0.0)
     ws.surface_rtprop_agendaSet(
         option="Specular_NoPol_ReflFix_SurfTFromt_surface")
 
-    # No sensor properties
-    ws.sensorOff()
-
+    
     # Atmosphere and surface
     ws.atm_fields_compact = atmfield
     ws.AtmosphereSet1D()
@@ -95,42 +80,37 @@ def calc_spectral_irradiance(atmfield,
 
     # Set surface height and temperature equal to the lowest atmosphere level
     ws.Extract(ws.z_surface, ws.z_field, 0)
-    ws.Extract(ws.t_surface, ws.t_field, 0)
+    ws.surface_skin_t = ws.t_field.value[0, 0, 0]
 
     # Output radiance not converted
     ws.StringSet(ws.iy_unit, "1")
 
-    # Definition of sensor position and LOS
-    ws.MatrixSet(ws.sensor_pos, np.array([[100e3]]))  # sensor in z = 100 km
-    ws.MatrixSet(ws.sensor_los,
-                 np.array([[180]
-                           ]))  # zenith angle: 0 looking up, 180 looking down
+    # set cloudbox to full atmosphere
+    ws.cloudboxSetFullAtm()
 
-    # Perform RT calculations
-    ws.propmat_clearsky_agendaAuto()
-    ws.lbl_checkedCalc()
+    # set particle scattering to zero, because we want only clear sky
+    ws.scat_data_checked = 1
+    ws.Touch(ws.scat_data)
+    ws.pnd_fieldZero()
+
+    # Check model atmosphere
+    ws.scat_data_checkedCalc()
     ws.atmfields_checkedCalc()
     ws.atmgeom_checkedCalc()
     ws.cloudbox_checkedCalc()
-    ws.sensor_checkedCalc()
+    ws.lbl_checkedCalc()
 
-    ws.AngularGridsSetFluxCalc(N_za_grid=nstreams,
-                               N_aa_grid=1,
-                               za_grid_type="double_gauss")
-
-    # calculate intensity field
-    ws.Tensor3Create("trans_field")
-    ws.spectral_radiance_fieldClearskyPlaneParallel(trans_field=ws.trans_field,
-                                                    use_parallel_za=0)
-    ws.spectral_irradiance_fieldFromSpectralRadianceField()
-
+    # Perform RT calculations
+    ws.spectral_irradiance_fieldDisort(nstreams=nstreams, emission=1)
+    
+    
     spectral_flux_downward = -ws.spectral_irradiance_field.value[:, :, 0, 0,
                                                                  0].copy()
     spectral_flux_upward = ws.spectral_irradiance_field.value[:, :, 0, 0,
                                                               1].copy()
 
-    spectral_flux_downward[np.isnan(spectral_flux_downward)] = 0.
-    spectral_flux_upward[np.isnan(spectral_flux_upward)] = 0.
+    # spectral_flux_downward[np.isnan(spectral_flux_downward)] = 0.
+    # spectral_flux_upward[np.isnan(spectral_flux_upward)] = 0.
 
     # set outputs
     f = ws.f_grid.value[:].copy()
@@ -198,3 +178,4 @@ def integrate_spectral_irradiance(f, spectral_flux, fmin=-np.inf, fmax=np.inf):
     flux = np.trapz(spectral_flux[logic, :], f[logic], axis=0)
 
     return flux
+
